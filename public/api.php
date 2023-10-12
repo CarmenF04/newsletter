33dc6c86883f5f972c73a955600470a435de6a1f
Carmen <33059@ma-web.nl>
2023-10-12 15:19:37 +0200

    database koppeling

--- public/api.php
+++ public/api.php
@@ -1,30 +1,25 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verkrijg de ingediende gegevens
     $email = $_POST["email"];
-    $uit_eten = isset($_POST["eten"]) ? 1 : 0;
-    $vervoer = isset($_POST["vervoer"]) ? 1 : 0;
+    $nieuwsbriefTypes = isset($_POST["nieuwsbriefType"]) ? $_POST["nieuwsbriefType"] : [];
 
-    // Verbind met de database (vervang 'gebruikersnaam' en 'wachtwoord' door je databasegegevens)
-    $conn = new mysqli("localhost", "gebruikersnaam", "wachtwoord", "mydatabase");
+    $conn = new mysqli("localhost", "db_user", "mijn_p@ss#", "newletter");
 
-    // Controleer op fouten bij het maken van de verbinding
     if ($conn->connect_error) {
         die("Databaseverbinding mislukt: " . $conn->connect_error);
     }
 
-    // Voeg de gegevens toe aan de database
-    $sql = "INSERT INTO reserveringen (email, eten, vervoer) VALUES ('$email', $uit_eten, $vervoer)";
+    foreach ($nieuwsbriefTypes as $nieuwsbriefType) {
+        $sql = "INSERT INTO nieuwsbrief_abonnementen (email, nieuwsbrief_type) VALUES ('$email', '$nieuwsbriefType')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Reservering is succesvol opgeslagen in de database.";
-    } else {
-        echo "Fout bij het opslaan van de reservering: " . $conn->error;
+        if ($conn->query($sql) !== TRUE) {
+            echo "Fout bij het opslaan van de reservering: " . $conn->error;
+        }
     }
 
-    // Sluit de databaseverbinding
+    echo "Nieuwsbriefabonnement(en) zijn succesvol opgeslagen in de database.";
+
     $conn->close();
 }
 ?>
-
--- public/index.php
+++ public/index.php
@@ -1,28 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="/public/main.js"></script>
 </head>
 
 <body>
-    <h1>Reservering</h1>
-    <form method="post" action="verwerk_reservering.php">
-        <label for="email">E-mail:</label>
-        <input type="email" name="email" required><br>
+    <h1>Schrijf je in voor onze nieuwsbrief</h1>
 
-        <label>Uit eten:</label>
-        <input type="radio" name="uit_eten" value="1"> Ja
-        <input type="radio" name="uit_eten" value="0"> Nee<br>
+    <form id="nieuwsbriefForm">
+        <label for="email">E-mailadres:</label>
+        <input type="email" id="email" name="email" required><br><br>
 
-        <label>Vervoer:</label>
-        <input type="radio" name="vervoer" value="1"> Ja
-        <input type="radio" name="vervoer" value="0"> Nee<br>
+        <label for="nieuwsbriefType">Kies nieuwsbrief type(s):</label><br>
+        <input type="checkbox" id="nieuwsbrief1" name="nieuwsbriefType" value="Nieuwsbrief 1"> Nieuwsbrief 1<br>
+        <input type="checkbox" id="nieuwsbrief2" name="nieuwsbriefType" value="Nieuwsbrief 2"> Nieuwsbrief 2<br>
 
-        <input type="submit" value="Reserveren">
+        <br><br>
+        <input type="submit" value="Inschrijven">
     </form>
+
+<!-- // javascript -->
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const nieuwsbriefForm = document.getElementById('nieuwsbriefForm');
+
+            nieuwsbriefForm.addEventListener('submit', function (event) {
+                event.preventDefault();
+
+                const email = document.getElementById('email').value;
+                const nieuwsbriefTypes = Array.from(document.getElementsByName('nieuwsbriefType'))
+                    .filter(input => input.checked)
+                    .map(input => input.value);
+
+                console.log('E-mail:', email);
+                console.log('Geselecteerde nieuwsbrieftypes:', nieuwsbriefTypes);
+
+            });
+        });
+    </script>
 </body>
 
 </html>
